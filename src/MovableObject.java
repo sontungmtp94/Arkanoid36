4aff0052649c61ae10a3e630807f2d9c2ed8bd3a
Sơn <vuanhson141106@gmail.com>
2025-10-05 20:04:45 +0700

    feat: [Sơn] (Thêm File MovableObject.java) Thêm abstract class MovableObject cho vật thể di chuyển trong game.

---
+++ src/MovableObject.java
@@ -0,0 +1,46 @@
+public abstract class MovableObject extends GameObject {
+    /** Vận tốc theo 2 trục x, y. */
+    private double dx;
+    private double dy;
+
+    /**
+     * Constructor cho MovableObject với vị trí, kích thước và tốc độ.
+     *
+     * @param x      Tọa độ x (ngang)
+     * @param y      Tọa độ y (dọc)
+     * @param width  Chiều rộng
+     * @param height Chiều cao
+     */
+    public MovableObject(int x, int y, int width, int height) {
+        super(x, y, width, height);
+        this.dx = 0;
+        this.dy = 0;
+    }
+
+    /** Cập nhật vị trí. */
+    @Override
+    public void update() {}
+
+    // Các getter và setter
+
+    public double getDx() {
+        return dx;
+    }
+
+    public void setDx(double dx) {
+        this.dx = dx;
+    }
+
+    public double getDy() {
+        return dy;
+    }
+
+    public void setDy(double dy) {
+        this.dy = dy;
+    }
+
+    public void setVelocity(double dx, double dy) {
+        this.dx = dx;
+        this.dy = dy;
+    }
+}
